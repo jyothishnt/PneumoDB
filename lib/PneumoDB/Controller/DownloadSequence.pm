687306c674b1da3dcfd88d9e64b0fdc83b285a3f
Jyothish <joyjoy.nt@gmail.com>
2016-01-26 12:26:17 +0000

    downlaod files code cleaning

--- lib/PneumoDB/Controller/DownloadSequence.pm
+++ lib/PneumoDB/Controller/DownloadSequence.pm
@@ -5,7 +5,6 @@ use JSON;
 use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
 use File::Spec;
 use File::Basename 'basename';
-use Data::Dumper;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -52,58 +51,53 @@ sub downloadSequenceFiles :Path('/download') {
     $c->res->body({err=>'Bad url!'});
   }
 
-
   $c->log->warn($log_str);
-  # Get type of url to retrieve - submitted_ftp or fastq_ftp
   my $file_arr = ();
-  # For each err id, get the ftp url and store in a hash of arrays
-  foreach my $row (@{$postData}) {
 
-    if($args[0]=~/assemblies/) {
-      push @$file_arr, File::Spec->catfile($c->config->{download_path}, $row->{study_id}, 'assemblies', $row->{lane_id}.'.contigs_velvet.fa.gz');
-    }
-    elsif($args[0]=~/annotations/) {
-      push @$file_arr, File::Spec->catfile($c->config->{download_path}, $row->{study_id}, 'annotations', $row->{lane_id}.'.gff.gz');
-    }
-  }
-print Dumper $file_arr;
   # Creating a zip file
   my $zip = Archive::Zip->new();
   my $found = 0;
-  foreach my $file (@$file_arr) {
-    my $file_name = basename $file;
+  my $file = '';
+  my $file_name;
+
+  foreach my $row (@{$postData}) {
+    if($args[0]=~/assemblies/) {
+      $file = File::Spec->catfile($c->config->{download_path}, $row->{study_id}, 'assemblies', $row->{lane_id}.'.contigs_velvet.fa.gz');
+      # If zipped not found, check if there is an unzipped one
+      if(! -s "$file") {
+        $file = File::Spec->catfile($c->config->{download_path}, $row->{study_id}, 'assemblies', $row->{lane_id}.'.contigs_velvet.fa');
+      }
+    }
+    elsif($args[0]=~/annotations/) {
+      $file = File::Spec->catfile($c->config->{download_path}, $row->{study_id}, 'annotations', $row->{lane_id}.'.gff.gz');
+      # If zipped not found, check if there is an unzipped one
+      if(! -s "$file") {
+        $file = File::Spec->catfile($c->config->{download_path}, $row->{study_id}, 'annotations', $row->{lane_id}.'.gff');
+      }
+    }
+
+    $file_name = basename $file;
     if(-s "$file") {
       $zip->addFile($file, $file_name, 1);
       $found = 1;
     }
   }
-  my $outfilename = "$args[0]_".$c->user->get('pnu_name')."_".time.".zip";
-  my $outfile = File::Spec->catfile($c->config->{dataviewer_tmp}, $outfilename);
-
   if($found == 0) {
-    $c->res->body({err=>'Files not available for download!'});
+    $c->res->body(to_json({err=>'Files not available for download!'}));
   }
   else {
     # Save the Zip file
+    my $outfilename = "$args[0]_".$c->user->get('pnu_username')."_".time.".zip";
+    my $outfile = File::Spec->catfile($c->config->{dataviewer_tmp}, $outfilename);
+
     unless ( $zip->writeToFileNamed($outfile) == AZ_OK ) {
       $c->res->body({err=>'Error creating zip file... Please try again!'});
     }
+
+    # return json data back to server
+    $c->res->header('Content-Type' => "text/json");
+    $c->res->body(to_json({'file'=>$outfile}));
   }
-
-# my $fh = IO::File->new( $outfile, 'r' );
-
-# binmode $fh;
-
-#  use File::stat;
-#  my $statObj = stat($outfile);
-   $c->res->header('Content-Type' => "text/json");
-#   # $c->res->header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-#   $c->res->header('Content-Length' => $statObj->size);
-#   $c->res->header('Content-Disposition' => "attachment;filename=$outfilename\n\n");
-#   # $c->finalize_headers();
-#   $c->res->body( $fh );
-  # return json data back to server
-  $c->res->body(to_json({'file'=>$outfile}));
 }
 
 
